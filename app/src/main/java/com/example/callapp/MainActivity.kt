ad30ef4379b65787ecd94d19f3f87029ccdfd1a2
Perez Mac <apple@Timring.local>
2020-08-06 20:01:21 +0100

    add layout coloring

--- app/src/main/java/com/example/callapp/MainActivity.kt
+++ app/src/main/java/com/example/callapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.callapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -13,14 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val phoneNumber = findViewById<EditText>(R.id.etNumbers).text
         val callButton = findViewById<Button>(R.id.btCall)
 
         callButton.setOnClickListener {
+            val phoneNumber = findViewById<EditText>(R.id.etNumbers).text
             if(phoneNumber.isBlank()) {
                 Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_SHORT).show()
             } else {
-
+                var intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("num:$phoneNumber")
+                startActivity(intent)
             }
         }
     }
